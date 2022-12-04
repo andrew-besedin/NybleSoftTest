504e763e79990fe971b7bf1f22ceb3a6ec6c48ea
andrewprog97 <andrewprog97@gmail.com>
2022-12-04 20:16:41 +0300

    fix data router name

--- src/routes/data.routes.ts
+++ src/routes/data.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import dataController from "../controllers/data.controller";
 
-const router = new (Router as any)();
+const dataRouter = new (Router as any)();
 
-router.post("/get-user-info", dataController.getUserInfo);
-router.put("/upload-image", dataController.uploadImage);
-router.post("/generate-pdf", dataController.generatePdf);
+dataRouter.post("/get-user-info", dataController.getUserInfo);
+dataRouter.put("/upload-image", dataController.uploadImage);
+dataRouter.post("/generate-pdf", dataController.generatePdf);
 
-export default router;
+export default dataRouter;
