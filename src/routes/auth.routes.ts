2b26dca4b846597354ae3d223e6d71e788df91a9
andrewprog97 <andrewprog97@gmail.com>
2022-12-07 19:01:38 +0300

    fix login request path

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import authController from "../controllers/auth.controller";
 const authRouter = new (Router as any)();
 
 authRouter.post("/register-user", authController.registerUser);
-authRouter.post("/login", authController.loginUser);
+authRouter.post("/login-user", authController.loginUser);
 authRouter.get("/verify-auth", authController.verifyAuth);
 authRouter.get("/get-auth-token", authController.getAuthToken);
 
